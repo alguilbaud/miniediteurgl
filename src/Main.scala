80d300b8d6a966ba088ddd608ff293f3772ef673
guilbaud.alexis@gmail.com <guilbaud.alexis@gmail.com@7c1bcca3-6601-a6a1-070c-f33dc0fae8b3>
2013-11-12 16:16:26 +0000

    v1 avec main

---
+++ src/Main.scala
@@ -0,0 +1,26 @@
+object Main {
+	def main(args: Array[String]) {
+		val buffer = new Buffer(new PressePapier)
+		val coller = new Coller(buffer)
+		val copier = new Copier(buffer)
+		val couper = new Couper(buffer)
+		val deplacer = new Deplacer(buffer)
+		val ecrire = new Ecrire(buffer)
+		val effacer = new Effacer(buffer)
+		val ihm = new IHM(buffer, coller, copier, couper, deplacer, ecrire, effacer)
+		
+		ihm.ecrire('a')
+		ihm.ecrire('b')
+		ihm.ecrire('c')
+		ihm.ecrire('d')
+		ihm.ecrire('e')
+		ihm.ecrire('f')
+		ihm.ecrire('g')
+		
+		
+		ihm.selectionner(4, 3)
+		ihm.deplacer(0)
+		ihm.afficherTexte
+		
+	}
+}
